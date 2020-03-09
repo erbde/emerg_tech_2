fe8201781accfc3c182690d92d3f9ee52a53ca23
Daniel Erb <erbde@vcu.edu>
2020-03-09 17:57:50 -0400

    Adds removal of punctuation and \n char

--- assignment_2.py
+++ assignment_2.py
@@ -7,22 +7,22 @@ import re
 # %%
 f = open("shakes.txt", "r+", encoding="UTF-8")
 # %%
-shakes = f.read() 
-# %%
+shakes = f.read()
 f.close()
-# %%
 shakes_words = shakes.split()
 #%%
-punct_list = punctuation.split(sep="", maxsplit=-1)
-punct_list = punctuation.split(sep="", maxsplit=-1)
-punctuation.
+punct_list = list(punctuation)
+
 # %%
 #table = print(shakes.maketrans(x = "", y = "", z = punctuation))
 print(shakes.maketrans(shakes,shakes,punctuation))
 
 # %%
-shakes.replace(punctuation,"",)
+new_shakes = shakes
+new_shakes = new_shakes.replace("\n", " ", -1)
+for i in punct_list:
+    new_shakes = new_shakes.replace(i, "", -1)
 # %%
-help(replace)
-
+new_shakes = new_shakes.lower()
+print(new_shakes)
 # %%
